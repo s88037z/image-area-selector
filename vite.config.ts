22b06b2408b422003af755d43d8d4233ae58350d
eric <s88037z@gmail.com>
2024-08-07 19:38:26 +0800

    feat(css):add @emotion,normalize.css,path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        presets: [
+          [
+            "@babel/preset-react",
+            { runtime: "automatic", importSource: "@emotion/react" },
+          ],
+        ],
+        plugins: ["@emotion"],
+      },
+    }),
+  ],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
 });
